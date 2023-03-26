5437daba3562f5ae9f4266de294ca22374547fea
hilibilibam <108792429+hilibilibam@users.noreply.github.com>
2023-03-26 12:57:32 +0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -13,11 +13,11 @@ output "vpc_cidr" {
   value = aws_vpc.project_vpc.cidr_block
 }
 
-output "private_subnet" {
-  value = aws_subnet.private_subnet.*.id
+output "private_subnets" {
+  value = aws_subnet.private_subnet
 }
-output "public_subnet" {
-  value = aws_subnet.public_subnet.*.id
+output "public_subnets" {
+  value = aws_subnet.public_subnet
 }
 
 output "igw-public" {
