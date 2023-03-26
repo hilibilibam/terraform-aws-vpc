06317ab07075dc6953ead6e4be08beeb6e879afe
hilibilibam <108792429+hilibilibam@users.noreply.github.com>
2023-03-26 12:06:21 +0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -14,30 +14,12 @@ output "vpc_cidr" {
 }
 
 output "private_subnets_id" {
-  value = aws_subnet.private_subnets.*.id
+  value = aws_subnet.private_subnet.*.id
 }
 output "public_subnets_id" {
-  value = aws_subnet.public_subnets.*.id
+  value = aws_subnet.public_subnet.*.id
 }
 
 output "igw-public" {
   value = aws_internet_gateway.igw_public
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#output "s3_bucket_arn" {
- # value       = aws_s3_bucket.terraform_state_file.arn
- # description = "The ARN of the S3 bucket"
-#}
