4553fce7a5cbc4c23f49c8db73d38aeb7cef0eb3
hilibilibam <108792429+hilibilibam@users.noreply.github.com>
2023-07-20 09:48:08 +0300

    Update main.tf with kubernetes adjustments
    Update main.tf with kubernetes adjustments  
    revisiting course after final delivery

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@
 # NETWORKING #
 
 resource "aws_vpc" "project_vpc" {
-  cidr_block                     = var.vpc_cidr
-  enable_dns_hostnames           = var.enable_dns_hostnames
-  enable_dns_support             = var.enable_dns_support
-  tags = {
+    cidr_block = var.vpc_cidr
+	  enable_dns_hostnames = true
+    enable_dns_support   = true
+	  tags = {
       "Name" = "${var.project_name}-vpc"
     }
 }
@@ -24,9 +24,13 @@ resource "aws_subnet" "private_subnet" {
   availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
   map_public_ip_on_launch = false
   tags = {
-      "Name" = "${var.project_name}-private-subnet${count.index + 1}-${regex(".$", data.aws_availability_zones.available.names[count.index])}"
+    "Name" = "${var.project_name}-private-subnet${count.index + 1}-${regex(".$", data.aws_availability_zones.available.names[count.index])}"
+    "kubernetes.io/cluster/hila-eks-kanduhila" = "shared"
+    "kubernetes.io/cluster/hila-eks-kanduhila" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1" 
     }
 }
+
 
 resource "aws_subnet" "public_subnet" {
   count                   = "${length(var.public_subnet)}"
@@ -35,7 +39,11 @@ resource "aws_subnet" "public_subnet" {
   availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
   map_public_ip_on_launch = true
   tags = {
-      "Name" = "${var.project_name}-public-subnet${count.index + 1}-${regex(".$", data.aws_availability_zones.available.names[count.index])}"
+    "Name" = "${var.project_name}-public-subnet${count.index + 1}-${regex(".$", data.aws_availability_zones.available.names[count.index])}"
+    "kubernetes.io/cluster/hila-eks-kanduhila" = "shared"
+    "kubernetes.io/cluster/hila-eks-kanduhila" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+
     }
 }
 
@@ -43,7 +51,7 @@ resource "aws_subnet" "public_subnet" {
 
 resource "aws_eip" "eip_nat_gateway" {
   vpc    = true
-  count = length(var.public_subnet)
+  count = 1 #length(var.public_subnet)
 
   tags = {
       "Name" = "${var.project_name}-eip-nat-gateway-${regex(".$", data.aws_availability_zones.available.names[count.index])}"
@@ -54,7 +62,7 @@ resource "aws_eip" "eip_nat_gateway" {
 # Create Nat Gateway
 
 resource "aws_nat_gateway" "nat_gateway" {
-  count         = length(var.public_subnet)
+  count         = 1 #length(var.public_subnet)
   allocation_id = aws_eip.eip_nat_gateway.*.id[count.index]
   subnet_id     = aws_subnet.public_subnet.*.id[count.index]
 
@@ -82,7 +90,7 @@ resource "aws_route_table_association" "public_route_asso" {
 resource "aws_route_table_association" "private_route_asso" {
   count          = length(aws_subnet.private_subnet) 
   subnet_id      = aws_subnet.private_subnet.*.id[count.index]
-  route_table_id = aws_route_table.rtb[count.index + 1].id
+  route_table_id = aws_route_table.rtb[1].id
 }
 
 
@@ -94,10 +102,9 @@ resource "aws_route" "route_public" {
 }
 
 resource "aws_route" "route_private" {
-  count                  = length(var.private_subnet)
-  route_table_id = aws_route_table.rtb.*.id[count.index + 1]
+  route_table_id = aws_route_table.rtb[1].id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id  = aws_nat_gateway.nat_gateway.*.id[count.index]
+  nat_gateway_id  = aws_nat_gateway.nat_gateway[0].id  #aws_nat_gateway.nat_gateway.*.id[count.index]
 
 }
 
